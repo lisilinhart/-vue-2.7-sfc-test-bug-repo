19fdc747a2d2bd87fd3ecb5cc870597c5b7a9be4
Lisi Linhart <ewl@storyblok.com>
2022-07-11 11:40:34 +0200

    fix: add composition API

---
+++ src/components/CompositionApi.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>{{ msg }} - {{ label }}</div>
+</template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+  props: {
+    msg: {
+      type: String,
+      default: ''
+    },
+  },
+  setup() {
+    const label = ref('init')
+
+    return {
+      label
+    }
+  },
+
+  beforeMount() {
+    this.label = 'mounted'
+  },
+}
+</script>
--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,5 +1,6 @@
 import { mount } from '@vue/test-utils'
 import RegularComponent from '@/components/RegularComponent.vue'
+import CompositionApi from '@/components/CompositionApi.vue'
 import SFCComponent from '@/components/SFCComponent.vue'
 import SFCComponentPlugin from '@/components/SFCComponentPlugin.vue'
 
@@ -7,6 +8,16 @@ describe('Regular Component', () => {
   it('renders options api', () => {
     const msg = 'new message'
     const wrapper = mount(RegularComponent, {
+      propsData: { msg }
+    })
+    expect(wrapper.html()).toMatch('new message - mounted')
+  })
+})
+
+describe('Composition API Component', () => {
+  it('renders composition api', () => {
+    const msg = 'new message'
+    const wrapper = mount(CompositionApi, {
       propsData: { msg }
     })
     expect(wrapper.html()).toMatch('new message - mounted')
