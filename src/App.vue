89d421e2063fa6247b4936037df82845948bc8ff
Lisi Linhart <ewl@storyblok.com>
2022-07-11 11:43:45 +0200

    feat: update examples

--- src/App.vue
+++ src/App.vue
@@ -2,19 +2,22 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <RegularComponent msg="Options API" />
-    <SFCComponent  msg="Composition API" />
+    <SFCComponent  msg="SFC Composition API" />
+    <CompositionApi  msg="Composition API" />
   </div>
 </template>
 
 <script>
 import RegularComponent from '@/components/RegularComponent.vue'
 import SFCComponent from '@/components/SFCComponent.vue'
+import CompositionApi from '@/components/CompositionApi.vue'
 
 export default {
   name: 'App',
   components: {
     RegularComponent,
-    SFCComponent
+    SFCComponent,
+    CompositionApi
   }
 }
 </script>
--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -2,7 +2,6 @@ import { mount } from '@vue/test-utils'
 import RegularComponent from '@/components/RegularComponent.vue'
 import CompositionApi from '@/components/CompositionApi.vue'
 import SFCComponent from '@/components/SFCComponent.vue'
-import SFCComponentPlugin from '@/components/SFCComponentPlugin.vue'
 
 describe('Regular Component', () => {
   it('renders options api', () => {
@@ -28,16 +27,6 @@ describe('SFC Component', () => {
   it('renders composition api', () => {
     const msg = 'new message'
     const wrapper = mount(SFCComponent, {
-      propsData: { msg }
-    })
-    expect(wrapper.html()).toMatch('new message - mounted')
-  })
-})
-
-describe('SFC Component Plugin', () => {
-  it('renders composition api', () => {
-    const msg = 'new message'
-    const wrapper = mount(SFCComponentPlugin, {
       propsData: { msg }
     })
     expect(wrapper.html()).toMatch('new message - mounted')
